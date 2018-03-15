a7b987c739c849184340b955e50f95aafab0e30f
osama-82 <sam.jawad1982@yahoo.com>
2018-03-15 13:00:43 -0500

    Change Osama To Memo

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -8,7 +8,7 @@ import { ProductService } from './product.service';
     styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-    pageTitle: string = 'Product List Osama';
+    pageTitle: string = 'Product List Memo';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
